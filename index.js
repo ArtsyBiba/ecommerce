98d995c942faedb23f7927c3cb4352f0de48ec85
Artsiom Biba <Artsiom.Biba@mktg.com>
2020-09-20 17:56:49 -0400

    updated script v3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	cookieSession({
-		keys : [ 'blahblahbla1238520983' ]
+		keys: ['blahblahbla1238520983']
 	})
 );
 app.use(authRouter);
@@ -20,6 +20,6 @@ app.use(adminProductsRouter);
 app.use(productsRouter);
 app.use(cartsRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log('listening');
 });
